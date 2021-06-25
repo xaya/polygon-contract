a539a01ae225503b3ba2bd864e45fa368d4306b6
Daniel Kraft <d@domob.eu>
2021-06-25 10:15:54 +0200

    Interface and test implementation for policy.
    The Xaya account registry contract will be non-upgradable, but it will
    have a configurable link to a policy contract.  This can be used to
    configure the policy for validating names and moves, as well as computing
    and distributing the WCHI fee for those operations.
    
    This commit adds the interface for the policy contract as well as an
    implementation for use in unit tests.

---
+++ contracts/IXayaPolicy.sol
@@ -0,0 +1,55 @@
+// SPDX-License-Identifier: MIT
+// Copyright (C) 2021 Autonomous Worlds Ltd
+
+pragma solidity ^0.8.6;
+
+/**
+ * @dev Interface for a contract that defines the validation and fee
+ * policy for Xaya accounts, as well as the NFT metadata returned for
+ * a particular name.  This contract is the "part" of the Xaya account
+ * registry that can be configured by the owner.
+ *
+ * All fees are denominated in WCHI tokens, this is not configurable
+ * by the policy (but instead coded into the non-upgradable parts
+ * of the account registry).
+ */
+interface IXayaPolicy
+{
+
+  /**
+   * @dev Returns the address to which fees should be paid.
+   */
+  function feeReceiver () external returns (address);
+
+  /**
+   * @dev Verifies if the given namespace/name combination is valid; if it
+   * is not, the function throws.  If it is valid, the fee that should be
+   * charged is returned.
+   */
+  function checkRegistration (string memory ns, string memory name)
+      external returns (uint256);
+
+  /**
+   * @dev Verifies if the given value is valid as a move for the given
+   * namespace.  If it is not, the function throws.  If it is, the fee that
+   * should be charged is returned.
+   *
+   * Note that the function does not know the exact name.  This ensures that
+   * the policy cannot be abused to censor specific names (and the associated
+   * game assets) after they have already been accepted for registration.
+   *
+   * FIXME: Should the policy (e.g. for fee) depend on the move's payment
+   * amount?  So that a certain fraction of each in-game payment can be
+   * charged as fee?
+   */
+  function checkMove (string memory ns, string memory mv)
+      external returns (uint256);
+
+  /**
+   * @dev Constructs and returns the token URI that should be returned
+   * as NFT metadata for a given name.
+   */
+  function tokenUriForName (string memory ns, string memory name)
+      external view returns (string memory);
+
+}
---
+++ contracts/TestPolicy.sol
@@ -0,0 +1,73 @@
+// SPDX-License-Identifier: MIT
+// Copyright (C) 2021 Autonomous Worlds Ltd
+
+pragma solidity ^0.8.6;
+
+import "./IXayaPolicy.sol";
+
+/**
+ * @dev An implementation of IXayaPolicy for use in unit tests, with
+ * very simple behaviour that allows to exercise all possible cases.
+ *
+ * THIS CONTRACT IS MEANT ONLY FOR TESTING, AND SHOULD NOT BE DEPLOYED
+ * OR USED IN PRODUCTION!
+ */
+contract TestPolicy is IXayaPolicy
+{
+
+  /** @dev The fee receiver address.  */
+  address public override feeReceiver;
+
+  /**
+   * @dev Constructs the contract, setting the policy receiver initially
+   * to the deploying account.
+   */
+  constructor ()
+  {
+    feeReceiver = msg.sender;
+  }
+
+  /**
+   * @dev Modifies the fee receiver address.  Note that this is callable
+   * by everyone without access checks, which is fine, since we use this
+   * contract only in testing.
+   */
+  function setFeeReceiver (address newReceiver) public
+  {
+    feeReceiver = newReceiver;
+  }
+
+  /**
+   * @dev Checks if a name registration is valid.  In this policy, they are
+   * valid if the namespace is non-empty.  The fee is 100 units per byte
+   * in the name.
+   */
+  function checkRegistration (string memory ns, string memory name)
+      external pure override returns (uint256)
+  {
+    require (bytes (ns).length > 0, "namespace must not be empty");
+    return 100 * bytes (name).length;
+  }
+
+  /**
+   * @dev Checks if a move is valid.  In this policy, a move is valid if
+   * it is not empty.  The fee is one unit per byte in the move data.
+   */
+  function checkMove (string memory, string memory mv)
+      external pure override returns (uint256)
+  {
+    require (bytes (mv).length > 0, "move data must not be empty");
+    return bytes (mv).length;
+  }
+
+  /**
+   * @dev Returns the metadata URI for a name.  This is just the
+   * name itself in the test policy.
+   */
+  function tokenUriForName (string memory, string memory name)
+      external pure override returns (string memory)
+  {
+    return name;
+  }
+
+}
