8cba82d54d20d7c71189af3ab9b3a2a313d8e083
Daniel Kraft <d@domob.eu>
2021-07-05 13:34:51 +0200

    Library for validating UTF-8 in Solidity.
    This adds a Solidity library for decoding individual codepoints from
    byte arrays and validating whole bytes sequences as UTF-8 per RFC 3629.

---
+++ contracts/Utf8.sol
@@ -0,0 +1,100 @@
+// SPDX-License-Identifier: MIT
+// Copyright (C) 2021 Autonomous Worlds Ltd
+
+pragma solidity ^0.8.6;
+
+/**
+ * @dev A Solidity library for validating UTF-8 from strings / bytes.
+ * This is based on the definition of UTF-8 in RFC 3629.
+ */
+library Utf8
+{
+
+  /**
+   * @dev Decodes the next codepoint from a byte array of UTF-8 encoded
+   * data.  The input is expected in the byte(s) following the offset
+   * into the array, and the return value is the decoded codepoint as well
+   * as the offset of the following bytes (if any).  If the input bytes
+   * are invalid, this method throws.
+   */
+  function decodeCodepoint (bytes memory data, uint offset)
+      internal pure returns (uint32 cp, uint newOffset)
+  {
+    require (offset < data.length, "no more input bytes available");
+
+    uint8 cur = uint8 (data[offset]);
+
+    /* Special case for ASCII characters.  */
+    if (cur < 0x80)
+      return (cur, offset + 1);
+
+    if (cur < 0xC0)
+      revert ("mid-sequence character at start of sequence");
+
+    /* Process the sequence-start character.  */
+    uint8 numBytes;
+    uint8 state;
+    if (cur < 0xE0)
+      {
+        numBytes = 2;
+        cp = uint32 (cur & 0x1F) << 6;
+        state = 6;
+      }
+    else if (cur < 0xF0)
+      {
+        numBytes = 3;
+        cp = uint32 (cur & 0x0F) << 12;
+        state = 12;
+      }
+    else if (cur < 0xF8)
+      {
+        numBytes = 4;
+        cp = uint32 (cur & 0x07) << 18;
+        state = 18;
+      }
+    else
+      revert ("invalid sequence start byte");
+    newOffset = offset + 1;
+
+    /* Process the following bytes of this sequence.  */
+    while (state > 0)
+      {
+        require (newOffset < data.length, "eof in the middle of a sequence");
+
+        cur = uint8 (data[newOffset]);
+        newOffset += 1;
+
+        require (cur & 0xC0 == 0x80, "expected sequence continuation");
+
+        state -= 6;
+        cp |= uint32 (cur & 0x3F) << state;
+      }
+
+    /* Verify that the character we decoded matches the number of bytes
+       we had, to prevent overlong sequences.  */
+    if (numBytes == 2)
+      require (cp >= 0x80 && cp < 0x800, "overlong sequence");
+    else if (numBytes == 3)
+      require (cp >= 0x800 && cp < 0x10000, "overlong sequence");
+    else if (numBytes == 4)
+      require (cp >= 0x10000 && cp < 0x110000, "overlong sequence");
+    else
+      revert ("invalid number of bytes");
+
+    /* Prevent characters reserved for UTF-16 surrogate pairs.  */
+    require (cp < 0xD800 || cp > 0xDFFF, "surrogate-pair character decoded");
+  }
+
+  /**
+   * @dev Validates that the given sequence of bytes is valid UTF-8
+   * as per the definition in RFC 3629.  Throws if not.
+   */
+  function validate (bytes memory data) internal pure
+  {
+    uint offset = 0;
+    while (offset < data.length)
+      (, offset) = decodeCodepoint (data, offset);
+    require (offset == data.length, "offset beyond string end");
+  }
+
+}
---
+++ contracts/Utf8TestHelper.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: MIT
+// Copyright (C) 2021 Autonomous Worlds Ltd
+
+pragma solidity ^0.8.6;
+
+import "./Utf8.sol";
+
+/**
+ * @dev Simple wrapper contract around the Utf8 library so we can call
+ * its internal functions from unit tests.
+ */
+contract Utf8TestHelper
+{
+
+  function decodeCodepoint (bytes memory data, uint offset)
+      public pure returns (uint32 cp, uint newOffset)
+  {
+    return Utf8.decodeCodepoint (data, offset);
+  }
+
+  function validate (bytes memory data) public pure
+  {
+    Utf8.validate (data);
+  }
+
+}
